91c4ea6510073ccb44221092c0ac85c200ab1502
Mrbexruz <vitusbehruzhp@gmail.com>
2025-06-09 17:18:24 +0500

    Drawer, ListView.builder,

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_drawer/home_pages.dart';
+import 'package:flutter_drawer/tab_bar_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,8 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePages(),
+    return  MaterialApp(
+      home: TabBarPage(),
     );
 
   }
